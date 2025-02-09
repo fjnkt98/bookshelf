5ec480644f7041c589ac38a009236d1a894d5607
fjnkt98 <fjnkt98@gmail.com>
2025-02-09 16:51:01 +0900

    ヘルスチェック機能の追加

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
+pub async fn health_check() -> axum::http::StatusCode {
+    axum::http::StatusCode::OK
+}
+
 async fn hello_world() -> &'static str {
     "Hello, World!"
 }
 
 #[tokio::main]
-async fn main() {
-    let app = axum::Router::new().route("/hello", axum::routing::get(hello_world));
+async fn main() -> anyhow::Result<()> {
+    let app = axum::Router::new()
+        .route("/hello", axum::routing::get(hello_world))
+        .route("/health", axum::routing::get(health_check));
     let addr = std::net::SocketAddr::new(std::net::Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await?;
 
     println!("Listening on {}", addr);
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
