41fdbd727d523333aad6c465423f02f9d5ce5361
fjnkt98 <fjnkt98@gmail.com>
2025-02-09 16:47:35 +0900

    サンプルプログラム

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = axum::Router::new().route("/hello", axum::routing::get(hello_world));
+    let addr = std::net::SocketAddr::new(std::net::Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
