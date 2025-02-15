c695a884391bc4f5ac6a640d878aa1abcf64ecdc
fjnkt98 <fjnkt98@gmail.com>
2025-02-15 20:01:47 +0900

    5.4.1 テーブル追加

--- adapter/migrations/20250209100754_start.up.sql
+++ adapter/migrations/20250209100754_start.up.sql
@@ -6,14 +6,36 @@ BEGIN
 END;
 ' LANGUAGE 'plpgsql';
 
+CREATE TABLE IF NOT EXISTS roles (
+    role_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role_id UUID NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+
+    FOREIGN KEY (role_id) REFERENCES roles (role_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TRIGGER users_updated_at_trigger BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
 CREATE TABLE IF NOT EXISTS books (
     book_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title TEXT NOT NULL,
     author TEXT NOT NULL,
     isbn TEXT NOT NULL,
     description TEXT NOT NULL,
+    user_id UUID NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT current_timestamp,
+
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TRIGGER books_updated_at_trigger BEFORE UPDATE ON books FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
