1271b0c13d365c283fcbc01fe865cd730764dd4e
fjnkt98 <fjnkt98@gmail.com>
2025-02-15 16:05:34 +0900

    5.3.2 ログ設定

---
+++ shared/src/env.rs
@@ -0,0 +1,19 @@
+#[derive(Default, strum::EnumString)]
+#[strum(serialize_all = "lowercase")]
+pub enum Environment {
+    #[default]
+    Development,
+    Production,
+}
+
+pub fn which() -> Environment {
+    #[cfg(debug_assertions)]
+    let default_env = Environment::Development;
+    #[cfg(not(debug_assertions))]
+    let default_env = Environment::Production;
+
+    match std::env::var("ENV") {
+        Err(_) => default_env,
+        Ok(v) => v.parse().unwrap_or(default_env),
+    }
+}
--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,6 +1,31 @@
+use anyhow::Context;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    init_logger()?;
     bootstrap().await
+}
+
+fn init_logger() -> anyhow::Result<()> {
+    let log_level = match shared::env::which() {
+        shared::env::Environment::Development => "debug",
+        shared::env::Environment::Production => "info",
+    };
+
+    let env_filter =
+        tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    let subscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false);
+
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(env_filter)
+        .try_init()?;
+
+    Ok(())
 }
 
 async fn bootstrap() -> anyhow::Result<()> {
@@ -12,14 +37,29 @@ async fn bootstrap() -> anyhow::Result<()> {
     let app = axum::Router::new()
         .merge(api::route::health::build_health_check_routers())
         .merge(api::route::book::build_book_routers())
+        .layer(tower_http::cors())
+        .layer(
+            tower_http::trace::TraceLayer::new_for_http()
+                .make_span_with(
+                    tower_http::trace::DefaultMakeSpan::new().level(tracing::Level::INFO),
+                )
+                .on_request(tower_http::trace::DefaultOnRequest::new().level(tracing::Level::INFO))
+                .on_response(
+                    tower_http::trace::DefaultOnResponse::new()
+                        .level(tracing::Level::INFO)
+                        .latency_unit(tower_http::LatencyUnit::Millis),
+                ),
+        )
         .with_state(registry);
 
     let addr = std::net::SocketAddr::new(std::net::Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = tokio::net::TcpListener::bind(addr).await?;
+    tracing::info!("Listening on {}", addr);
 
-    println!("Listening on {}", addr);
-
-    axum::serve(listener, app).await?;
-
-    Ok(())
+    axum::serve(listener, app)
+        .await
+        .context("Unexpected error happened in server")
+        .inspect_err(
+            |e| tracing::error!(error.cause_chain = ?e, error.message = %e, "Unexpected error"),
+        )
 }
